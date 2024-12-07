c6756e069f9f04aed41f311f3de0d3b0080c75f7
Annica <annicarberg@gmail.com>
2024-12-07 14:52:34 +0900

    Add variables, math operators, and operate functions

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// variables for number, operator, another number
+let num1;
+let num2;
+let operator;
+
+
+// add, subtract, multiply, divide functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// operate function for taking operator and number then calling above function
+
+function operate(operator, num1, num2) {
+    if (operator = '+') {
+        add(num1, num2); 
+    } else if (operator = '-') {
+        subtract(num1, num2);
+    } else if (operator = '*') {
+        multiple(num1, num2);
+    } else if (operator = '/') {
+        divide(num1, num2)
+    }
+}
+
