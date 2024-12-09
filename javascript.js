105a3682c305b152e48423b7014aa1e54cde0e24
Annica <annicarberg@gmail.com>
2024-12-10 08:43:22 +0900

    Add DOM elements and EventListeners for buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 let num1;
 let num2;
 let operator;
+const operandBtn = document.querySelectorAll(".operand");
+const operatorBtn = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+const zeroBtn = document.querySelector(".zero");
+const equalsBtn = document.querySelector(".equals");
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -31,3 +37,32 @@ function operate(operator, num1, num2) {
             divide(num1, num2);
     }
 }
+
+// show contents of operand button 
+operandBtn.forEach(button => {
+    button.addEventListener("click", () => {
+    alert(`${button.innerHTML}`);
+    });
+});
+
+// show contents of operator button
+operatorBtn.forEach(button => {
+    button.addEventListener("click", () => {
+    alert(`${button.innerHTML}`);
+    });
+});
+
+// show contents of equals, zero, clear button
+clearBtn.addEventListener("click", () => {
+    alert(`${clearBtn.innerHTML}`);
+});
+
+zeroBtn.addEventListener("click", () => {
+    alert(`${zeroBtn.innerHTML}`);
+});
+
+equalsBtn.addEventListener("click", () => {
+    alert(`${equalsBtn.innerHTML}`);
+});
+    
+
